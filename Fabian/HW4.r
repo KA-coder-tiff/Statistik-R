001ebce63dacacb9e68ac826adc9fd20c40662a0
Hartmann Fabian <e11824496@student.tuwien.ac.at>
2019-10-31 12:19:19 +0100

    start HW4.r

---
+++ Fabian/HW4.r
@@ -0,0 +1,18 @@
+# AUFGABE 1
+
+# a) 
+diff(pnorm(c(84,116),100,16))
+
+# b)
+qnorm(0.025,100,16)
+qnorm(0.025,100,16, lower.tail = FALSE)
+
+
+
+# AUFGABE 2
+
+# a) 
+pbinom(260,600,3/4) - pbinom(240,600,3/4)
+600*3/4
+
+# b)
