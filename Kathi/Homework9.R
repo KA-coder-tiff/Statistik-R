b946868b4504af44b0f3f957988f4015706712e8
Katharina Scheucher <e11809620@student.tuwien.ac.at>
2019-12-09 20:36:07 +0100

    small changes

--- Kathi/Homework9.R
+++ Kathi/Homework9.R
@@ -1,3 +1,17 @@
+# b) double check with anova
+# Gruppengroessen als Vektor (k Gruppen)
+ni <- c(4,4,4)
+# Daten als Vektor , erst alle aus Gruppe 1, dann Gruppe 2 usw.
+g1 = c(2,3,3,4)
+g2 = c(3,4,4,5)
+g3 = c(4,5,5,6)
+x <- c(g1,g2, g3)
+# Gruppen als Faktorvariable
+gr <- factor(rep(1:3,ni))
+# ANOVA durchfuehren (F-Test)
+anova(aov(x~gr))
+
+
 #(2) Processor - Part 1
 #(a)
 load("/Users/Kathi/Desktop/TU/3. Semester/Statistik & Wahrscheinlichkeit/Repo/Statistik-R/temperatures.Rdata")
@@ -13,7 +27,7 @@ for (i in seq(1,5)) {
   segments(m+sem,i+0.3,m+sem,i-0.3, col = "red")
 }
 
-#(3) Processor - Part 1
+#(3) Processor - Part 2
 m = 0
 n = 0
 for (i in seq(1, length(temp))) {
@@ -37,5 +51,29 @@ f
 
 p = 1-pf(f,length(temp)-1, n-length(temp))
 p 
+# b) double check with anova
+# Gruppengroessen als Vektor (k Gruppen)
+ni <- c(13,15,24,11,14)
+# Daten als Vektor , erst alle aus Gruppe 1, dann Gruppe 2 usw.
+x <- c(temp[[1]],temp[[2]], temp[[3]], temp[[4]], temp[[5]])
+# Gruppen als Faktorvariable
+gr <- factor(rep(1:5,ni))
+# ANOVA durchfuehren (F-Test)
+anova(aov(x~gr))
+
+#(4) Processor - Part 3
+#(a)
+m = choose(length(temp), 2)
+#(b)
+for (i in seq(1, length(temp)-1)) {
+  for (j in seq(i+1, length(temp))) {
+    xi = temp[[i]]
+    xj = temp[[j]]
+    p = t.test(xi, xj)$p.value
+    cat("Test ", i, " and ", j, "; without correction:", p, ";\t with correction:", p*m, "\n")
+  }
+}
+
+
 
 
